2223c4d75f42bc225399270d27253e5d7b26d494
RaymanMoha <105370124+RaymanMoha@users.noreply.github.com>
2022-10-30 12:49:19 +0300

    Create amenity.py

---
+++ models/amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This file contain the class Amenity"""
+
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """
+    Amenity class that inherits from BaseModel class
+    """
+    name = ""
