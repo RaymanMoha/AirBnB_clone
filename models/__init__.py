50c4846292edb5240fedfcdaa7558f1a27661dbc
RaymanMoha <105370124+RaymanMoha@users.noreply.github.com>
2022-10-30 12:46:45 +0300

    Create __init__.py

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Module that executes each time that models package is imported"""
+from models.engine.file_storage import FileStorage
+
+storage = FileStorage()
+storage.reload()
